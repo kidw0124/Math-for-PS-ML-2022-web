405b46278b70d71bd3bbdd39a82294c441b6b678
kidw0124 <kidw0124@naver.com>
2022-05-09 16:30:04 +0900

    메인 변경

--- back/app.js
+++ back/app.js
@@ -1,41 +1,54 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var responseTime = require("response-time");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+	responseTime((req, res, time) => {
+		console.log(`----------------------------------------`);
+		console.log(`new request: ${req.originalUrl}`);
+		console.log(`${req.method} ${req.url} ${time}ms`);
+		console.log(`headers: ${JSON.stringify(req.headers)}`);
+		console.log(`body: ${JSON.stringify(req.body)}`);
+		console.log(`query: ${JSON.stringify(req.query)}`);
+		console.log(`params: ${JSON.stringify(req.params)}`);
+		console.log(`----------------------------------------`);
+	})
+);
+app.use("/api", indexRouter);
+app.use("/api/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render("error");
 });
 
 module.exports = app;
+
