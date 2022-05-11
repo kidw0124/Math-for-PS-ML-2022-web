58f9de4f69c2f9c0aff96987410c309cfa17bdf2
kidw0124 <kidw0124@naver.com>
2022-05-11 22:23:55 +0900

    이름 MatKor로 변경

--- back/routes/db.js
+++ back/routes/db.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mysql = require("mysql");
 const dbconfig = require("./../config/database.config.js").dbconfig;
-const mathkor = mysql.createConnection({ ...dbconfig, database: "mathkor" });
+const mathkor = mysql.createConnection({ ...dbconfig, database: "MatKor" });
 console.log(mathkor);
 
 module.exports = { router };
