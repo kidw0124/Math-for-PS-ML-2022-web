986e6dfa484b3af4a0594df89f432c06f0d69025
kidw0124 <kidw0124@naver.com>
2022-05-11 17:16:58 +0900

    const 변경

--- back/routes/problem.js
+++ back/routes/problem.js
@@ -1,5 +1,5 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
 router.get("/title/:problemNumber", function (req, res, next) {
 	res.send("problem " + req.params.problemNumber);
