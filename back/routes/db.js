5a62fae726e8a887f361f08dd2c34b200472bf05
kidw0124 <kidw0124@naver.com>
2022-05-11 17:17:10 +0900

    db, login 생성

---
+++ back/routes/db.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const mysql = require("mysql");
+const dbconfig = require("./../config/database.config.js").dbconfig;
+const mathkor = mysql.createConnection({ ...dbconfig, database: "mathkor" });
+console.log(mathkor);
+
+module.exports = { router };
