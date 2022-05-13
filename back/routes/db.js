ce044f2153249c50ff02da4bbb2d567ad5802e9c
kidw0124 <kidw0124@naver.com>
2022-05-13 12:47:49 +0900

    사용자 정보 삽입, 검색

--- back/routes/db.js
+++ back/routes/db.js
@@ -2,7 +2,45 @@ const express = require("express");
 const router = express.Router();
 const mysql = require("mysql");
 const dbconfig = require("./../config/database.config.js").dbconfig;
-const mathkor = mysql.createConnection({ ...dbconfig, database: "MatKor" });
-console.log(mathkor);
+const matkor = mysql.createConnection({ ...dbconfig, database: "MatKor" });
 
-module.exports = { router };
+matkor.connect((err) => {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log("Connected to MatKor");
+	}
+});
+
+const insertUser = (user) => {
+	return new Promise((resolve, reject) => {
+		matkor.query(
+			"INSERT INTO users (id, password, name, email, last_login, register_date) VALUES (?, ?, ?, ?, null, now())",
+			[user.id, user.password, user.name, user.email],
+			function (err, result) {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(result);
+				}
+			}
+		);
+	});
+};
+const selectUser = (id) => {
+	return new Promise((resolve, reject) => {
+		matkor.query(
+			"SELECT * FROM users WHERE id = ?",
+			[id],
+			function (err, result) {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(result);
+				}
+			}
+		);
+	});
+};
+
+module.exports = { router, insertUser, selectUser };
