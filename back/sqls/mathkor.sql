2489d1b5b9ad225954efe99f8b076e7085b92bc6
kidw0124 <kidw0124@naver.com>
2022-05-13 12:46:47 +0900

    세션 table 생성

--- back/sqls/mathkor.sql
+++ back/sqls/mathkor.sql
@@ -12,3 +12,21 @@ CREATE TABLE IF NOT EXISTS problems (
     `subtask_path` TEXT
 );
 ALTER TABLE problems AUTO_INCREMENT = 1000;
+CREATE TABLE IF NOT EXISTS users (
+    `uid` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `id` TEXT NOT NULL,
+    `password` TEXT NOT NULL,
+    `name` TEXT NOT NULL,
+    `email` TEXT NOT NULL,
+    `last_login` DATETIME,
+    `register_date` DATETIME NOT NULL
+);
+ALTER TABLE users AUTO_INCREMENT = 0;
+CREATE TABLE IF NOT EXISTS sessions (
+    `sid` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `uid` INT NOT NULL,
+    `expire` DATETIME NOT NULL,
+    `ip` TEXT NOT NULL,
+    `session_key` TEXT NOT NULL,
+    FOREIGN KEY (uid) REFERENCES users(uid) ON UPDATE CASCADE
+);
