0568ddd77a3e3639cd8f1bdd4ff853ed47dcffa6
kidw0124 <kidw0124@naver.com>
2022-05-20 15:31:03 +0900

    랭킹페이지

---
+++ front/src/Rank.js
@@ -0,0 +1,9 @@
+import "./Rank.css";
+import { Navigate, useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function Rank() {
+	return <h1>순위는 아직 준비되지 않았습니다</h1>;
+}
+export default Rank;
