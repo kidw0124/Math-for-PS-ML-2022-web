8ef74b5852fc7df498d57e226556e5258779efc7
kidw0124 <kidw0124@naver.com>
2022-05-29 00:04:43 +0900

    문제 내용 전송

--- back/routes/problem.js
+++ back/routes/problem.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const db = require("./db");
+const fs = require("fs");
 
 router.get("/title/:problemNumber", function (req, res, next) {
 	const problemNumber = req.params.problemNumber;
@@ -11,6 +12,25 @@ router.get("/title/:problemNumber", function (req, res, next) {
 			res.send(probdt[0].title);
 		}
 	});
+});
+
+router.post("/content/:problemNumber", async function (req, res, next) {
+	const problemNumber = req.params.problemNumber;
+	const problem = await db.getProblem(problemNumber);
+	if (problem.length === 0) {
+		res.status(404).send("Page not found");
+	} else {
+		const problemPath = `./problems/${problem[0].path}/${problem[0].number}.md`;
+		fs.readFile(problemPath, "utf8", function (err, data) {
+			if (err) {
+				console.log(err);
+				res.status(500).send("Internal server error");
+			} else {
+				console.log(data);
+				res.send(data);
+			}
+		});
+	}
 });
 
 router.get("/list", function (req, res, next) {
